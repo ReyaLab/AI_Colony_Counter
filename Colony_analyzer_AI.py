03f665e87e96a0a55a371a952e4247c3aaaa32c1
ReyaLab <reyalabcu@gmail.com>
2025-06-30 19:16:56 -0400

    Update Colony_analyzer_AI.py

--- Colony_analyzer_AI.py
+++ Colony_analyzer_AI.py
@@ -221,7 +221,7 @@ def main(args):
     img_map = cut_img(path, file)
     from transformers import SegformerForSemanticSegmentation
     # Load fine-tuned model
-    model = SegformerForSemanticSegmentation.from_pretrained(args[4]+"General_Purpose_Colony_ImagerV1")  # Adjust pat
+    model = SegformerForSemanticSegmentation.from_pretrained(args[4]+"Segformer_Colony_Counter_GP")  # Adjust pat
     model.to(device)
     model.eval()  # Set to evaluation mode
     for z in img_map:
